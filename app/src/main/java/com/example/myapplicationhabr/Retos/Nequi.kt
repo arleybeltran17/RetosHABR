271dae19a8b762061b3c6a091ad87fffb687a64b
Arley Beltran <arleybeltran17@gmail.com>
2023-07-25 11:34:35 -0500

    realizando el reto nequi por favor terminar el reto :3

--- app/src/main/java/com/example/myapplicationhabr/Retos/Nequi.kt
+++ app/src/main/java/com/example/myapplicationhabr/Retos/Nequi.kt
@@ -4,61 +4,91 @@ class Nequi {
 }
 
 fun main() {
+
+
     val Telefono:Long=3222672926
     var Contra:Long=2547
 
-    println("Ingrese Su Numero De Telefono")
-    var tel2:Long= readln()!!.toLong()
+    var opcion:String="si"
 
-    println("Ingrese Su Contraseña")
-    var contra2:Long= readln()!!.toLong()
+    while (opcion=="si"){
 
-    if(tel2==Telefono ){
-        if (contra2==Contra){
 
-            println("Bienvenido A Nuestros Servicios Nequi")
+        println("Ingrese Su Numero De Telefono")
+        var tel2:Long= readln()!!.toLong()
 
-            var saldo:Int=4500
-            println("Tu Saldo Es De: $saldo")
+        println("Ingrese Su Contraseña")
+        var contra2:Long= readln()!!.toLong()
 
-            println(" Que Accion Desea Realizar \n1.Saca \n 2.Envia \n 3.Recarga \n 4.Salir ")
-            var opcion:Int= readln()!!.toInt()
+        if(tel2==Telefono ){
+            if (contra2==Contra){
 
-            if (opcion==1){
+                println("Bienvenido A Nuestros Servicios Nequi")
 
-                println("Has Seleccionado Sacar Plata A Continuacion Se Mostrara Tu Saldo: $saldo")
-                println("Ingresa La Cantidad Que Deseas Retirar")
-                var saca:Int= readln()!!.toInt()
+                var saldo:Int=4500
+                println("Tu Saldo Es De: $saldo")
 
-                if (saca<=saldo){
+                println(" Que Accion Desea Realizar \n1.Saca \n 2.Envia \n 3.Recarga \n 4.Salir ")
+                var opcion:Int= readln()!!.toInt()
 
-                    var newsaldo=saldo-saca
-                    println("Retiro Exitoso Por Favor Reclama Tu Dinero")
-                    println("Tu Saldo Restante Es De: $newsaldo")
+                if (opcion==1){
 
-                }else{
-                    println("Saldo No Disponible")
+                    println("Has Seleccionado Sacar Plata A Continuacion Se Mostrara Tu Saldo: $saldo")
+
+                    var codigo: Int =(100000..999999).random()
+
+                    println("Tu Codigo Es: $codigo")
+
+                    println("Ingresa Tu Codigo")
+                    var codigo2:Int= readln()!!.toInt()
+
+                    if (codigo2==codigo){
+
+                        println("Ingresa La Cantidad Que Deseas Retirar")
+                        var saca:Int= readln()!!.toInt()
+
+                        if (saca<=saldo) {
+
+                            var newsaldo = saldo - saca
+                            println("Retiro Exitoso Por Favor Reclama Tu Dinero")
+                            println("Tu Saldo Restante Es De: $newsaldo")
+                        }else{
+                            println("Saldo No Disponible")
+                        }
+                    }else{
+                        println("Codigo No Valido, Vuelva A Intentar")
+                    }
+
+                }else if(opcion==2){
+
+                    println("Ingrese El Numero De La Persona A La Cual Desea Enviar")
+                    var tel2:Long= readln()!!.toLong()
+
+
+                }else if(opcion==3){
+
+                }else if(opcion==4){
+
                 }
 
-            }else if(opcion==2){
 
-            }else if(opcion==3){
 
-            }else if(opcion==4){
-
+            }else{
+                println("¡Upps! Parece que tus datos de acceso\n" +
+                        "no son correctos, Tienes tres intentos más")
             }
 
-
-
         }else{
+
             println("¡Upps! Parece que tus datos de acceso\n" +
                     "no son correctos, Tienes tres intentos más")
+
         }
 
-    }else{
 
-        println("¡Upps! Parece que tus datos de acceso\n" +
-                "no son correctos, Tienes tres intentos más")
-
+        println("Desea Realizar Otra Opcion? si o no?")
+        opcion= readln()
     }
+
+
 }
